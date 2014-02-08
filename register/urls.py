9e1db920e1f37e643dcd88bbe0ea37904b999664
Jonathan Canaveral <neumerance@neudev.(none)>
2014-02-08 12:02:26 +0800

    fixing the gitignore

--- register/urls.py
+++ register/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = patterns('',
     url(r'^$', RegistrationView.as_view(), name='register'),
+
     url(r'^request/activation/$', RequestActivation.as_view(), name='request_activation'),
     url(r'^activate/$', ActivationView.as_view(), name='activate'),
 
